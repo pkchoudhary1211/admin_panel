a89a2fcbdb5d9798c209f4a23eb2976eb4247e16
prakash choudhary <pkchoudhary1211@gmail.com>
2019-04-28 11:46:32 +0530

    Add all my files

---
+++ vendor/laravel/passport/src/Http/Controllers/ConvertsPsrResponses.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Laravel\Passport\Http\Controllers;
+
+use Illuminate\Http\Response;
+
+trait ConvertsPsrResponses
+{
+    /**
+     * Convert a PSR7 response to a Illuminate Response.
+     *
+     * @param  \Psr\Http\Message\ResponseInterface  $psrResponse
+     * @return \Illuminate\Http\Response
+     */
+    public function convertResponse($psrResponse)
+    {
+       // dd($psrResponse);
+        return new Response(
+            $psrResponse->getBody(),
+            $psrResponse->getStatusCode(),
+            $psrResponse->getHeaders()
+        );
+    }
+}
